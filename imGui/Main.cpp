e6db773738cff74ae9e6db0d1565fa7bb2851de2
TheLeyter <v.lopatinets@ukr.net>
2021-06-10 18:52:21 +0300

    Init imGui

--- imGui/Main.cpp
+++ imGui/Main.cpp
@@ -10,18 +10,24 @@
 
 HINSTANCE g_hInstance = NULL;
 HWND g_hWnd = NULL;
-int g_iWindowWidth = 800;
-int g_iWindowHeigh = 600;
+int g_iWindowWidth = 1200;
+int g_iWindowHeigh = 800;
 bool g_bAppicationState = true;
-IDirect3D9* g_pDirect3D = NULL;
-IDirect3DDevice9* g_pDirect3DDevice = NULL;
+bool imGuiMain = true;
+
+
+LPDIRECT3D9 g_pD3D = NULL;
+LPDIRECT3DDEVICE9 g_pd3dDevice = NULL;
+D3DPRESENT_PARAMETERS g_d3dpp = {};
+ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
 
 #pragma region FuncPrototype
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevIntance, LPSTR lpCmdLine, int iCmdShow);
 LRESULT CALLBACK WndProc(HWND hWnd, UINT iMsg, WPARAM wParam, LPARAM lParam);
 bool CreateDeviceD3D(HWND hWnd);
-
+void CleanupDeviceD3D();
+void ResetDevice();
 
 #pragma endregion
 
@@ -70,13 +76,30 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevIntance, LPSTR lpCmdLine,
 		return 0;
 	}
 
+
+	MSG msg;
+	ZeroMemory(&msg, sizeof(msg));
+
+	if (!CreateDeviceD3D(g_hWnd))
+	{
+		CleanupDeviceD3D();
+		::UnregisterClass(wc.lpszClassName, wc.hInstance);
+		return 1;
+	}
+
 	ShowWindow(g_hWnd, SW_SHOW);
 	UpdateWindow(g_hWnd);
 	SetFocus(g_hWnd);
 	SetForegroundWindow(g_hWnd);
 
-	MSG msg;
-	ZeroMemory(&msg, sizeof(msg));
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	ImGuiIO& io = ImGui::GetIO(); (void)io;
+
+	ImGui::StyleColorsDark();
+
+	ImGui_ImplWin32_Init(g_hWnd);
+	ImGui_ImplDX9_Init(g_pd3dDevice);
 
 	while (g_bAppicationState) {
 
@@ -85,13 +108,53 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevIntance, LPSTR lpCmdLine,
 			TranslateMessage(&msg);
 			DispatchMessage(&msg);
 		}
+
+		ImGui_ImplDX9_NewFrame();
+		ImGui_ImplWin32_NewFrame();
+		ImGui::NewFrame();
+
+		if (imGuiMain) {
+			ImGui::ShowDemoWindow(&imGuiMain);
+		}
+
+		ImGui::EndFrame();
+		g_pd3dDevice->SetRenderState(D3DRS_ZENABLE, false);
+		g_pd3dDevice->SetRenderState(D3DRS_ALPHABLENDENABLE, false);
+		g_pd3dDevice->SetRenderState(D3DRS_SCISSORTESTENABLE, false);
+		D3DCOLOR clear_col_dx = D3DCOLOR_RGBA((int)(clear_color.x * clear_color.w * 255.0f), (int)(clear_color.y * clear_color.w * 255.0f), (int)(clear_color.z * clear_color.w * 255.0f), (int)(clear_color.w * 255.0f));
+		g_pd3dDevice->Clear(0, NULL, D3DCLEAR_TARGET | D3DCLEAR_ZBUFFER, clear_col_dx, 1.0f, 0);
+		if (g_pd3dDevice->BeginScene() >= 0)
+		{
+			ImGui::Render();
+			ImGui_ImplDX9_RenderDrawData(ImGui::GetDrawData());
+			g_pd3dDevice->EndScene();
+		}
+		HRESULT result = g_pd3dDevice->Present(NULL, NULL, NULL, NULL);
+
+		// Handle loss of D3D9 device
+		if (result == D3DERR_DEVICELOST && g_pd3dDevice->TestCooperativeLevel() == D3DERR_DEVICENOTRESET)
+			ResetDevice();
+
 	}
+	ImGui_ImplDX9_Shutdown();
+	ImGui_ImplWin32_Shutdown();
+	ImGui::DestroyContext();
+
+	CleanupDeviceD3D();
+	::DestroyWindow(g_hWnd);
+	::UnregisterClass(wc.lpszClassName, wc.hInstance);
+
 	return 0;
 
 }
 
+extern IMGUI_IMPL_API LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
+
 LRESULT CALLBACK WndProc(HWND hWnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
 {
+	if (ImGui_ImplWin32_WndProcHandler(hWnd, iMsg, wParam, lParam))
+		return true;
+
 	switch (iMsg)
 	{
 		case WM_DESTROY:
@@ -100,8 +163,69 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
 			return 0;
 		}
 
+		case WM_SIZE:
+		{
+			if (g_pd3dDevice != NULL && wParam != SIZE_MINIMIZED)
+			{
+				g_d3dpp.BackBufferWidth = LOWORD(lParam);
+				g_d3dpp.BackBufferHeight = HIWORD(lParam);
+				ResetDevice();
+			}
+
+			return 0;
+		}
+		case WM_SYSCOMMAND: {
+			if ((wParam & 0xfff0) == SC_KEYMENU) { // Disable ALT application menu
+				return 0;
+			}
+		}
+
 	}
 
 	return DefWindowProc(hWnd, iMsg, wParam, lParam);
 }
+
+bool CreateDeviceD3D(HWND hWnd)
+{
+
+	if((g_pD3D = Direct3DCreate9(D3D_SDK_VERSION)) == NULL)
+		return false;
+
+	ZeroMemory(&g_d3dpp, sizeof(g_d3dpp));
+	g_d3dpp.Windowed = true;
+	g_d3dpp.SwapEffect = D3DSWAPEFFECT_DISCARD;
+	g_d3dpp.BackBufferFormat = D3DFMT_UNKNOWN;
+	g_d3dpp.EnableAutoDepthStencil = true;
+	g_d3dpp.AutoDepthStencilFormat = D3DFMT_D16;
+	g_d3dpp.PresentationInterval = D3DPRESENT_INTERVAL_ONE;
+
+	if (g_pD3D->CreateDevice(D3DADAPTER_DEFAULT, D3DDEVTYPE_HAL, hWnd, D3DCREATE_HARDWARE_VERTEXPROCESSING, &g_d3dpp, &g_pd3dDevice) < 0)
+		return false;
+
+	return true;
+}
+
+void CleanupDeviceD3D()
+{
+	if (g_pd3dDevice) {
+		g_pd3dDevice->Release();
+		g_pd3dDevice = NULL;
+	}
+	if (g_pD3D)
+	{ 
+		g_pD3D->Release();
+		g_pD3D = NULL;
+	}
+}
+
+void ResetDevice()
+{
+	ImGui_ImplDX9_InvalidateDeviceObjects();
+	HRESULT hr = g_pd3dDevice->Reset(&g_d3dpp);
+	if (hr == D3DERR_INVALIDCALL)
+		IM_ASSERT(0);
+	ImGui_ImplDX9_CreateDeviceObjects();
+}
+
+
 
